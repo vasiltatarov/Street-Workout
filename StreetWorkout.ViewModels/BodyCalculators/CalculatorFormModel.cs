1cf1e1c718d90b9c3106949f2210057f0e775dd5
vasiltatarov <vasiltatarov3@gmail.com>
2021-07-29 16:44:45 +0300

    Implemented body calculator

---
+++ StreetWorkout.ViewModels/BodyCalculators/CalculatorFormModel.cs
@@ -0,0 +1,20 @@
+﻿namespace StreetWorkout.ViewModels.BodyCalculators
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CalculatorFormModel
+    {
+        [Range(30, 250)]
+        public byte Weight { get; set; }
+
+        [Range(50, 250)]
+        public byte Height { get; set; }
+
+        public double Activity { get; set; }
+
+        [Range(10, 100)]
+        public byte Age { get; set; }
+
+        public int Gender { get; set; }
+    }
+}
---
+++ StreetWorkout/Controllers/BodyCalculatorsController.cs
@@ -0,0 +1,52 @@
+﻿namespace StreetWorkout.Controllers
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Data.Models.Enums;
+    using ViewModels.BodyCalculators;
+
+    public class BodyCalculatorsController : Controller
+    {
+        public IActionResult Calculate()
+            => this.View();
+
+        [HttpPost]
+        public IActionResult Calculate(CalculatorFormModel model)
+        {
+            if (!Enum.IsDefined(typeof(Gender), model.Gender))
+            {
+                this.ModelState.AddModelError(nameof(model.Gender), "Invalid Gender type.");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var gender = (Gender) model.Gender;
+
+            var genderValue = gender == Gender.Male ? 66 : 655.1;
+            var weightInPounds = model.Weight * 2.20462262;
+            var centimetersInInches = model.Height * 0.393700787;
+
+            // Basal Metabolic Rate (BMR)
+            double bmr;
+
+            if (gender == Gender.Male)
+            {
+                bmr = genderValue + (6.2 * weightInPounds) + (12.7 * centimetersInInches) - (6.76 * model.Age);
+            }
+            else
+            {
+                bmr = genderValue + (4.35 * weightInPounds) + (4.7 * centimetersInInches) - (4.7 * model.Age);
+            }
+
+            var calories = (int)(bmr * model.Activity);
+
+            this.ViewData["Calories"] = calories;
+
+            return this.View(model);
+        }
+    }
+}
