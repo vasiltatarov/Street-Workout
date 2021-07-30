7b030ff60a711d2f832f662b45c7f1ae31b8cfdb
vasiltatarov <vasiltatarov3@gmail.com>
2021-07-31 00:47:36 +0300

    tested body calculator and comment services

---
+++ StreetWorkout.Test/Services/BodyCalculatorServiceTest.cs
@@ -0,0 +1,23 @@
+﻿namespace StreetWorkout.Test.Services
+{
+    using Xunit;
+    using Data.Models.Enums;
+    using StreetWorkout.Services.BodyCalculators;
+
+    public class BodyCalculatorServiceTest
+    {
+        [Fact]
+        public void CalculateCaloriesShouldCalculateCaloriesAndReturnDoubleValue()
+        {
+            // Arrange
+            var bodyCalculatorService = new BodyCalculatorService();
+
+            // Act
+            var result = bodyCalculatorService.CalculateCalories(80, 180, 1.55, 21, Gender.Male);
+
+            // Assert
+            Assert.IsType<double>(result);
+            Assert.Equal(2972, result);
+        }
+    }
+}
---
+++ StreetWorkout.Test/Services/CommentServiceTest.cs
@@ -0,0 +1,44 @@
+﻿namespace StreetWorkout.Test.Services
+{
+    using System.Linq;
+    using Xunit;
+    using Data.Models;
+    using Mocks;
+    using StreetWorkout.Services.Comments;
+    using ViewModels.Comments;
+
+    public class CommentServiceTest
+    {
+        [Theory]
+        [InlineData("test", "vs1", 1)]
+        [InlineData("test1", "vs2", 3)]
+        public void AddMethodShouldAddNewCommentToDatabase(string content, string userId, int workoutId)
+        {
+            const string username = "vasko";
+            const string imageUrl = "vasko.png";
+
+            // Arrange
+            var data = DatabaseMock.Instance;
+            data.Users.Add(new ApplicationUser
+            {
+                Id = userId,
+                UserName = username,
+                ImageUrl = imageUrl,
+            });
+            data.SaveChanges();
+
+            var commentService = new CommentService(data);
+
+            // Act
+            var result = commentService.Add(content, userId, workoutId);
+
+            // Assert
+            Assert.IsType<CommentResponseModel>(result);
+            Assert.NotNull(result);
+            Assert.Equal(1, data.Comments.Count());
+            Assert.Equal(username, result.UserUsername);
+            Assert.Equal(imageUrl, result.UserImageUrl);
+            Assert.Equal(content, result.Content);
+        }
+    }
+}
