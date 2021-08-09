088f68838c7f94a40ab3c1de981cb4f9eeb95eab
vasil tatarov <vasiltatarov3@gmail.com>
2021-08-09 12:03:04 +0300

    added administration home page

---
+++ StreetWorkout/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿namespace StreetWorkout.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static WebConstants;
+
+    [Authorize(Roles = AdministratorRoleName)]
+    public class HomeController : AdministrationController
+    {
+        public IActionResult Index()
+            => this.View();
+    }
+}
