a5aca5a5b95821320e47db8bfa90e1522a2932ec
vasil tatarov <vasiltatarov3@gmail.com>
2021-08-09 13:28:17 +0300

    separated supplement categories view

---
+++ StreetWorkout/Areas/Administration/Controllers/SupplementCategoriesController.cs
@@ -0,0 +1,21 @@
+﻿namespace StreetWorkout.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    using Services.Supplements;
+
+    using static WebConstants;
+
+    [Authorize(Roles = AdministratorRoleName)]
+    public class SupplementCategoriesController : AdministrationController
+    {
+        private readonly ISupplementService supplements;
+
+        public SupplementCategoriesController(ISupplementService supplements)
+            => this.supplements = supplements;
+
+        public IActionResult All()
+            => this.View(this.supplements.GetSupplementCategories());
+    }
+}
