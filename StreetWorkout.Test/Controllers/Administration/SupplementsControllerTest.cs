dca32852e5540606c76595d7af2f41049e8268d3
vasil tatarov <vasiltatarov3@gmail.com>
2021-08-16 16:34:38 +0300

    writed more integrations tests

---
+++ StreetWorkout.Test/Controllers/Administration/SupplementsControllerTest.cs
@@ -0,0 +1,118 @@
+﻿namespace StreetWorkout.Test.Controllers.Administration
+{
+    using MyTested.AspNetCore.Mvc;
+    using Shouldly;
+    using Xunit;
+
+    using StreetWorkout.Areas.Administration.Controllers;
+    using ViewModels.Supplements;
+    using StreetWorkout.Data.Models;
+
+    using static WebConstants;
+
+    public class SupplementsControllerTest
+    {
+        [Fact]
+        public void CreateShouldReturnViewWithCorrectFormModel()
+            => MyController<SupplementsController>
+                .Instance()
+                .WithUser(user => user
+                    .InRole(AdministratorRoleName))
+                .Calling(c => c.Create())
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<SupplementFormModel>());
+
+        [Theory]
+        [InlineData("contentTest", "https://addons.cdn.mozilla.net/user-media/previews/full/230/230000.png?modified=1622132702", "amixTest")]
+        public void CreateShouldReturnRedirectToActionWithValidModelStateAlsoShouldBeAllowedOnlyByPostRequest(
+            string content,
+            string imageUrl,
+            string name)
+            => MyController<SupplementsController>
+                .Instance()
+                .WithUser(user => user
+                    .InRole(AdministratorRoleName))
+                .WithData(data => data
+                    .WithEntities(new SupplementCategory()))
+                .Calling(c => c.Create(new SupplementFormModel
+                {
+                    Name = name,
+                    CategoryId = 1,
+                    Content = content,
+                    ImageUrl = imageUrl,
+                    Price = 50,
+                    Quantity = 500,
+                }))
+                .ShouldHave()
+                .ActionAttributes(attr => attr
+                    .RestrictingForHttpMethod(HttpMethod.Post))
+                .AndAlso()
+                .ShouldReturn()
+                .RedirectToAction("All");
+
+        [Theory]
+        [InlineData("contentTest", "miss", "amixTest")]
+        public void CreateShouldReturnViewWhenModelStateIsInvalidAlsoShouldBeAllowedOnlyByPostRequest(
+            string content,
+            string imageUrl,
+            string name)
+            => MyController<SupplementsController>
+                .Instance()
+                .WithUser(user => user
+                    .InRole(AdministratorRoleName))
+                .WithData(data => data
+                    .WithEntities(new SupplementCategory()))
+                .Calling(c => c.Create(new SupplementFormModel
+                {
+                    Name = name,
+                    CategoryId = 1,
+                    Content = content,
+                    ImageUrl = imageUrl,
+                }))
+                .ShouldHave()
+                .ActionAttributes(attr => attr
+                    .RestrictingForHttpMethod(HttpMethod.Post))
+                .AndAlso()
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<SupplementFormModel>()
+                    .Passing(data =>
+                    {
+                        data.Name.ShouldBe(name);
+                        data.Content.ShouldBe(content);
+                    }));
+
+        [Theory]
+        [InlineData("contentTest", "https://addons.cdn.mozilla.net/user-media/previews/full/230/230000.png?modified=1622132702", "amixTest")]
+        public void CreateShouldReturnViewWhenCategoryIdIsNotValidAlsoShouldBeAllowedOnlyByPostRequest(
+            string content,
+            string imageUrl,
+            string name)
+            => MyController<SupplementsController>
+                .Instance()
+                .WithUser(user => user
+                    .InRole(AdministratorRoleName))
+                .Calling(c => c.Create(new SupplementFormModel
+                {
+                    CategoryId = 1,
+                    Name = name,
+                    Content = content,
+                    ImageUrl = imageUrl,
+                    Price = 50,
+                    Quantity = 500,
+                }))
+                .ShouldHave()
+                .ActionAttributes(attr => attr
+                    .RestrictingForHttpMethod(HttpMethod.Post))
+                .AndAlso()
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<SupplementFormModel>()
+                    .Passing(data =>
+                    {
+                        data.Name.ShouldBe(name);
+                        data.Content.ShouldBe(content);
+                    }));
+    }
+}
---
+++ StreetWorkout.Test/Controllers/Administration/WorkoutPaymentsControllerTest.cs
@@ -0,0 +1,25 @@
+﻿namespace StreetWorkout.Test.Controllers.Administration
+{
+    using System.Collections.Generic;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+
+    using StreetWorkout.Areas.Administration.Controllers;
+    using StreetWorkout.Services.WorkoutPayments.Models;
+
+    using static WebConstants;
+
+    public class WorkoutPaymentsControllerTest
+    {
+        [Fact]
+        public void AllShouldReturnViewWithWorkoutsModel()
+            => MyController<WorkoutPaymentsController>
+                .Instance()
+                .WithUser(user => user
+                    .InRole(AdministratorRoleName))
+                .Calling(c => c.All())
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<IEnumerable<UserWorkoutPaymentServiceModel>>());
+    }
+}
